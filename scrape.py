4dda7eae911b9fcc0ccbc2127541e5475ba3791c
hubersh <hunterhubers@gmail.com>
2019-04-03 22:54:41 -0400

    Finished web scraping script

--- scrape.py
+++ scrape.py
@@ -8,38 +8,68 @@ from bs4 import BeautifulSoup
 
 
 class WebScrape:
-    """Designed to scrape web pages."""
-    def __init__(self, page):
-        # TODO Add function docstring
+    """Designed to scrape web pages"""
+    def __init__(self):
+        """Construct class variables"""
+        self.data = []
+        self.page = None
+        self.soup = None
+
+    def get_page(self, page):
+        """Set the page and soup variables to a URL's contents
+
+        :param page: A website URL
+        :return: None
+        """
         self.page = requests.get(page)
         # Create a BeautifulSoup object
         self.soup = BeautifulSoup(self.page.text, 'html.parser')
 
-    def scrape_content(self):
-        # TODO determine if divs is good enough
-        paragraphs = self.soup.find_all('p')
-        divs = self.soup.find_all('div')
+    def set_data(self):
+        """Pull all of the text from a web page and store it in a class variable
 
-        self.export_data('output.txt', divs)
-        # TODO Loop though data and clear out unneeded divs
-        # for text in divs:
-        # TODO Implement regex cleaning
-        #     print(text)
+        :return: None
+        """
+        self.data.append(re.findall(r'(?<=>)([^{\n]*?)\w?(?=<)', str(self.soup)))
 
     def clean_content(self):
-        # TODO Implement NLP cleaning
-        pass
+        """Clean the text from a web page
+
+        :return: Cleaned web page text data
+        """
+        output = []
+        for item in self.data:
+            for content in item:
+                if content == '':
+                    continue
+                elif len(content.split()) < 5:
+                    continue
+                else:
+                    output.append(content)
+        return output
 
     @staticmethod
     def export_data(filename, data):
-        # TODO Try to make method generic
+        """Export the text data from a web page to a file
+
+        :param filename: Name of the file to write to
+        :param data: Data to be written to the file
+        :return: None
+        """
         with open(filename, 'w+') as F:
-            for text in data:
-                F.write(str(text)+"\n")
+            F.write(str(data)+"\n")
 
 
 if __name__ == '__main__':
+
     # Initialize the class
-    S = WebScrape(sys.argv[1])
-    S.scrape_content()
+    S = WebScrape()
+
+    # Pull the page data
+    S.get_page(sys.argv[1])
+    S.set_data()
+
+    # Clean and export the data
+    text_data = S.clean_content()
+    S.export_data('output3.txt', text_data)
 
