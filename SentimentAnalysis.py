2691e2370fa353ee348e73b1dc30d7f6eda4d70c
huttonb <huttonb@mail.gvsu.edu>
2019-04-09 15:32:50 -0400

    started work on pickling of sentimentanalysis
    Can't access tokenizer through pickling from a seperate class because pickle doesn't save that data, have to make it available to PredictBias class

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -8,6 +8,7 @@ import numpy as np
 import os
 import pandas as pd
 import pyprind
+import pickle
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -61,10 +62,10 @@ if __name__ == '__main__':
 
     # Create a training and test set.
     # This is a simplified approach.
-    X_train = df.loc[:350, 'Article'].values
-    y_train = df.loc[:350, 'Directional-Bias'].values
-    X_test = df.loc[350:, 'Article'].values
-    y_test = df.loc[350:, 'Directional-Bias'].values
+    X_train = df.loc[:559, 'Article'].values
+    y_train = df.loc[:559, 'Directional-Bias'].values
+    X_test = df.loc[559:, 'Article'].values
+    y_test = df.loc[559:, 'Directional-Bias'].values
 
     tfidf = TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None)
 
@@ -85,19 +86,20 @@ if __name__ == '__main__':
                   ]
     lr_tfidf = Pipeline([('vect', tfidf), ('clf', LogisticRegression(random_state=0))])
 
-    gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, scoring='accuracy', cv=2, verbose=1, n_jobs=1)
+    gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, scoring='accuracy', cv=5, verbose=1, n_jobs=1)
 
     # This fitting process takes a long time!
     gs_lr_tfidf.fit(X_train, y_train)
+    pickle.dump(gs_lr_tfidf, open('pickled_model.sav', 'wb'))
 
     # print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
     print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
 
     clf = gs_lr_tfidf.best_estimator_
-   # print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
+    #print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
 
     # TODO Read https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
     # TODO Call scrape.external_call() this return will be the article text string that needs to be predicted
     X = scrape.external_call("https://www.reuters.com/article/us-usa-immigration-asylum/u-s-judge-halts-trump-policy-of-returning-asylum-seekers-to-mexico-idUSKCN1RK2E6")
     #TODO create temporary file to send to predict, as it needs a file not a string. Or work with streams for it.
-    gs_lr_tfidf.predict(tempfile)  # TODO Figure out how to pass new data to the prediction function
+   # gs_lr_tfidf.predict(tempfile)  # TODO Figure out how to pass new data to the prediction function
